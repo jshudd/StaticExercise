1a0fd4937ea99e083e6c2ce27eaf6218f736c3fc
jshudd <jshudd@gmail.com>
2020-04-15 13:35:01 -0500

    finished exercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the temperature in F you want to convert?");
+            double fahTemp = Convert.ToDouble(Console.ReadLine());
+            double convertToCel = Convert.ToDouble(TempConverter.FahrenheitToCelsius(fahTemp));
+
+
+            Console.WriteLine($"That equals {convertToCel} degrees Celsius");
+            Console.WriteLine();
+
+            Console.WriteLine("What is the temperature in Celsius you want to convert?");
+            double celTemp = Convert.ToDouble(Console.ReadLine());
+            double convertToFah = Convert.ToDouble(TempConverter.CelsisuToFahrenheit(celTemp));
+
+            Console.WriteLine($"That equals {convertToFah} degrees Fahrenheit");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+            double x = 5;
+            double y = 9;
+            var celTemp = ((fTemp - 32) * (x / y));
+            return celTemp;
+        }
+
+        public static double CelsisuToFahrenheit(double cTemp)
+        {
+            double x = 5;
+            double y = 9;
+            var fahTemp = ((cTemp * (y / x)) + 32);
+            return fahTemp;
+        }
+
+    }
+}
